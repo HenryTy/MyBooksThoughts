cb4c9f359a5a0d10324c190c5be1b09b4e3957b2
Kuba <jakub.k.borowiak@student.put.poznan.pl>
2020-07-21 12:57:43 +0200

    menu error correction

--- app/src/main/java/com/example/mybooksthoughts/Keys.kt
+++ app/src/main/java/com/example/mybooksthoughts/Keys.kt
@@ -1,7 +1,7 @@
 package com.example.mybooksthoughts
 
 object Keys {
-    val CLIENT_ID = ""
-    val CLIENT_SECRET = ""
-    val API_KEY = ""
+    val CLIENT_ID = "263421571386-49050990raaujknop4i7t8dt8k8sqnri.apps.googleusercontent.com"
+    val CLIENT_SECRET = "2fdNcRN1pWO50o4r8L0eRh2H"
+    val API_KEY = "AIzaSyA8ayugxrRe0671yBipVb1wt0Tvu2WfrLQ"
 }
